9bbf4f543ce9b7a2f61e0e64768265f7d4d3dc4a
Angelica Sanchez <anlsanchez@ucdavis.edu>
2018-10-15 23:35:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,7 @@ class Song
   extend Memorable
   extend Findable
   extend Paramable
-  
+
   attr_accessor :name
   attr_reader :artist
 
@@ -14,10 +14,6 @@ class Song
     @@songs << self
   end
 
-  #def self.find_by_name(name)
-  #  @@songs.detect{|a| a.name == name}
-  #end
-
   def self.all
     @@songs
   end
@@ -25,8 +21,4 @@ class Song
   def artist=(artist)
     @artist = artist
   end
-
-  #def to_param
-  #  name.downcase.gsub(' ', '-')
-  #end
 end
